30728e53646529fbe5180d20b9d4e496aa1270f7
Zenek Palmer <zenek.palmer@fivebyfiveuk.com>
2013-06-10 16:07:40 +0100

    config updates to allow for environment sepcific binary locations

---
+++ application/config/app.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+  'gm_path' => '/usr/local/bin gm ', // server config
+  'ffmpeg_path' => '/usr/bin', // server config
+);
